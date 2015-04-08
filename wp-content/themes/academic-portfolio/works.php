3a744250e93a551e4b8ee234284543042d4a2804
Jay Renteria <jay@jayrenteria.com>
2015-04-08 15:54:22 -0600

    added works page

---
+++ wp-content/themes/academic-portfolio/works.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+Template Name: Works
+*/
+
+?>
+
+<?php get_header(); ?>
+
+	<div class="wrapper">
+
+		<div class="container-fluid" id="contact_section">
+			<div class="row">
+				<div class="col-md-12 mt60 mb60">
+					<h2>Works</h2>
+				</div>
+			</div><!-- end row -->
+		</div><!-- end container-fluid -->
+
+		<div class="push"></div>
+	</div>
+
+<?php get_footer(); ?>
