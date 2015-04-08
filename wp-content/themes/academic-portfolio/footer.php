30092823f14851a832341a1c3d199f55e4c0ec50
Alonso Indacochea <aindaco1@hotmail.com>
2015-04-08 15:12:55 -0600

    updated contact styling

--- wp-content/themes/academic-portfolio/footer.php
+++ wp-content/themes/academic-portfolio/footer.php
@@ -4,7 +4,7 @@
 			<div class="row-fluid">
 				<div class="col-sm-12">
 					<div class="vertical-spacer-xs"></div>
-					<p>&copy; <?php bloginfo('name'); echo " " . date('Y'); ?></p>
+					<p class="footer-text">&copy; <?php bloginfo('name'); echo " " . date('Y'); ?></p>
 				</div>
 			</div>
 		</div>
