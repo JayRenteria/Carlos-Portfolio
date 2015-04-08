8a1bb3cf258c9a6001ea06bbc895287eb4ba64ef
Jay Renteria <jay@jayrenteria.com>
2015-04-08 16:16:08 -0600

    added mt60 to the top container

--- wp-content/themes/academic-portfolio/single.php
+++ wp-content/themes/academic-portfolio/single.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 
-<div class="container-fluid">
+<div class="container-fluid mt60">
   <div class="row">
     <div class="col-sm-8">
       <?php if( have_posts()): while( have_posts()): the_post(); ?>
